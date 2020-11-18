737cbec341761ecede2416bff50d2f126374efc6
kobe benson <kobebenson15@gmail.com>
2020-11-17 20:39:56 -0700

    adding files

---
+++ index.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+console.log("hello world".red);
+console.log("hello world".blue);
+console.log("hello world".yellow);
+console.log("hello world".magenta);
+console.log("hello world".black);
